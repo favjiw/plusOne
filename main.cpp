637ee0894f26da6c755ee7600cc5e9504c83091a
favjiw <mfavianj@gmail.com>
2025-07-23 11:37:42 +0700

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> numbers = {1, 2, 3, 4, 5};
+    int result = 0;
+    // for
+    return 0;
+}
