e8a725998194723b1a743205d94133ce30a0b9b2
favjiw <mfavianj@gmail.com>
2025-07-23 13:21:29 +0700

    problem solved but can be better

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    vector<int> numbers = {1, 2, 3, 4, 5};
+    vector<int> digits = {1, 2, 3, 4, 5};
     int result = 0;
-    // for
+    for(int digit:digits){
+        result = result * 10 + digit;
+    }
+
+    result++;
+    cout << "The number formed by the digits is: " << result << endl;
+    cout << "The size of the vector is: " << digits.size() << endl;
+    vector<int> resultDigits;
+    while(result > 0){
+        resultDigits.push_back(result % 10);
+        result /= 10;
+    }
+    reverse(resultDigits.begin(), resultDigits.end());
+
+    for(int resultDigit: resultDigits){
+        cout << resultDigit << " ";
+    }
     return 0;
 }
